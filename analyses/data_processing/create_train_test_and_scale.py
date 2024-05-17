cf7f60f43567ea1923be6e978eeca77262f6f631
Parth2504-Patel <ar20791@bristol.ac.uk>
2024-05-17 09:44:13 +0100

    Fixed small typos

--- analyses/data_processing/create_train_test_and_scale.py
+++ analyses/data_processing/create_train_test_and_scale.py
@@ -13,15 +13,15 @@ from sklearn.preprocessing import StandardScaler
 bankruptcy_df = pd.read_csv("../../data/raw/Taiwanese_Bankruptcy_Dataset.csv") # relative file paths used, aligning with data science principles
 
 bankruptcy_df.columns = bankruptcy_df.columns.str.strip() # As noticed in Initial_Data_Exploration notebook, column names have whitespace at the start
-bankruptcy_df = bankruptcy_df.rename(columns={"Bankrupt?" : "Bankrupt_Status"}) # Change target variable to something more clear, Easier to handle and more interpretable
-bankruptcy_df = bankruptcy_df.drop("Net Income Flag", axis=1) # Noted to drop in data exploration
+bankruptcy_df = bankruptcy_df.rename(columns={"Bankrupt?" : "bankrupt_status"}) # Change target variable to something more clear, Easier to handle and more interpretable
+bankruptcy_df = bankruptcy_df.drop("Net Income Flag", axis=1) # Noted to drop in data exploration, since column only takes one value (no information provided from this)
 
 # get input predictors and output variable
-all_input_predictors = bankruptcy_df.drop("Bankrupt_Status", axis=1)
-target_data = bankruptcy_df["Bankrupt_Status"]
+all_input_predictors = bankruptcy_df.drop("bankrupt_status", axis=1)
+target_data = bankruptcy_df["bankrupt_status"]
 
 # Perform stratified split (80-20 train test split), since its initial data exploration shows there is a class imbalance issue with small amount of data for minority class 
-X_train, X_test, Y_train, Y_test = train_test_split(
+x_train, x_test, y_train, y_test = train_test_split(
     all_input_predictors, target_data,
     test_size = 0.2, random_state = 1853006, 
     shuffle=True, stratify=target_data
@@ -29,8 +29,8 @@ X_train, X_test, Y_train, Y_test = train_test_split(
 
 ## Performing data scaling
 # Get continuous features to scale, Liability-Assests Flag is only input binary variable in dataset
-continuous_X_train = X_train.drop(["Liability-Assets Flag"], axis=1)
-continous_X_test = X_test.drop(["Liability-Assets Flag"], axis=1)
+continuous_X_train = x_train.drop(["Liability-Assets Flag"], axis=1)
+continous_X_test = x_test.drop(["Liability-Assets Flag"], axis=1)
 
 # Performing scaling on (continous) features
 standard_scalar = StandardScaler()
@@ -44,15 +44,15 @@ test_dataset = pd.DataFrame(continuous_X_test_scaled, columns=continous_X_test.c
 
 # Add in the continous features, binary variable and the target variable to form a full dataset
 full_train_dataset = pd.concat([
-    Y_train.reset_index(drop = True), # add target v,
+    y_train.reset_index(drop = True), # add target v,
     train_dataset, # add the continous features,
-    X_train["Liability-Assets Flag"].reset_index(drop=True), # add back the binary variable
+    x_train["Liability-Assets Flag"].reset_index(drop=True), # add back the binary variable
 ] , axis=1)
 
 full_test_dataset = pd.concat([
-    Y_test.reset_index(drop = True), # add target v,
+    y_test.reset_index(drop = True), # add target v,
     test_dataset, # add the continous features,
-    X_test["Liability-Assets Flag"].reset_index(drop=True), # add back the binary variable
+    x_test["Liability-Assets Flag"].reset_index(drop=True), # add back the binary variable
 ] , axis=1)
 
 # Write to CSV files
