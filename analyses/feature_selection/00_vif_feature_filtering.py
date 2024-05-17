4e1b8c53d10b53651b579dc6add5186f38ef34af
Parth2504-Patel <ar20791@bristol.ac.uk>
2024-05-17 10:14:17 +0100

    Perform VIF Analysis filtering

---
+++ analyses/feature_selection/00_vif_feature_filtering.py
@@ -0,0 +1,28 @@
+'''
+This file is stage one of two for the feature selection / filtering process to narrow down the total variables to a smaller subset of variables.
+The first stage performs a Variance Inflation Factor(VIF) analysis, and filters out variables that have a VIF value greater than a threshold, set to 5.
+'''
+
+# Imports
+import pandas as pd
+from statsmodels import api
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+# Read in train dataset created
+train_dataset = pd.read_csv("../../data/derived/scaled_train_dataset.csv") # relative file pathing 
+all_x = train_dataset.drop("bankrupt_status", axis=1) # drop target variable, not used in vif analysis
+
+all_x_with_const = api.add_constant(all_x, prepend=True) # add intercept column (as first column) to perform VIF calculations
+
+## Calculate VIF scores
+all_x_with_columns = all_x_with_const.columns
+num_of_variables = len(all_x_with_columns)
+vif_df = pd.DataFrame(all_x_with_columns.drop("const"), columns=["feature_name"])
+
+vif_df["vif_score"] = [variance_inflation_factor(all_x_with_const.values, i) for i in range(num_of_variables)][1:] # list comprehension to efficiently calculate VIF score for each column
+
+## Filter out features that have score higher than threshold (set to 5 as suggested by literature)
+vif_threshold = 5
+vif_filtered_features = vif_df[vif_df["vif_score"] < vif_threshold]["feature_name"]
+
+vif_filtered_features.to_csv("../../outputs/feature_selection/feature_lists/vif_filtered_features_list.txt", index=False) # write filtered features to text file. Forms a simple documentation of changes also.
