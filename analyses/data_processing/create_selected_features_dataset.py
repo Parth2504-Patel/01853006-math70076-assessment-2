c74971ca5098508855f9dd14fe351ab45ebb03f7
Parth2504-Patel <ar20791@bristol.ac.uk>
2024-05-20 02:35:09 +0100

    Clean select final features

--- analyses/data_processing/create_selected_features_dataset.py
+++ analyses/data_processing/create_selected_features_dataset.py
@@ -4,14 +4,26 @@ This file creates the train and train set with the selected features obtained fr
 
 # Imports
 import pandas as pd
-train_datset = pd.read_csv("../../data/derived/scaled_balanced_train_dataset.csv") # read in train
-test_dataset = pd.read_csv("../../data/derived/scaled_test_dataset.csv") # Read in test data
+import os
 
-final_selected_features = pd.read_csv("../../outputs/feature_selection/feature_lists/random_forest_filtered_features_list.txt")["feature_name"].tolist() # Read in the final selected features, and get corresponding columns from the datasets
-final_selected_features_and_target = final_selected_features +  ["bankrupt_status"] # get all input and target variables
+def read_select_write_dataset(inital_path, output_path):
+    dataset = pd.read_csv(inital_path)
+    final_selected_features = pd.read_csv("../../outputs/feature_selection/feature_lists/random_forest_filtered_features_list.txt")["feature_name"].tolist() # Read in the final selected features, and get corresponding columns from the datasets
+    final_selected_features_and_target = final_selected_features +  ["bankrupt_status"] # get all input and target variables
+    selected_dataset = dataset[final_selected_features_and_target]
+    selected_dataset.to_csv(output_path, index=False)
 
-selected_features_train_set = train_datset[final_selected_features_and_target] # get corresponding variables from initial test split
-selected_features_test_set = test_dataset[final_selected_features_and_target] # get corresponding variables from initial test split
+root_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")) # obtain path of root folder
+dervied_data_path = os.path.join(root_folder, "data", "derived") 
 
-selected_features_train_set.to_csv("../../data/derived/selected_features_train_dataset.csv", index=False) # write this subsetted dataset to csv, so models can read this in directly.
-selected_features_test_set.to_csv("../../data/derived/selected_features_test_dataset.csv", index=False) # write this subsetted dataset to csv, so models can read this in directly.
+# Train dataset
+read_select_write_dataset(
+    inital_path= os.path.join(dervied_data_path, "balanced_scaled_train_dataset.csv"),
+    output_path= os.path.join(dervied_data_path, "selected_features_train_dataset.csv")
+)
+
+# Train dataset
+read_select_write_dataset(
+    inital_path= os.path.join(dervied_data_path, "scaled_test_dataset.csv"),
+    output_path= os.path.join(dervied_data_path, "selected_features_test_dataset.csv")
+)
